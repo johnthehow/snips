767979a8317bce087bd3d55fd764da38f66483f5
John Thehow <johnthehow@qq.com>
2023-03-21 18:03:15 +0800

    regular update

--- printer.py
+++ printer.py
@@ -5,10 +5,11 @@ res = inspect.stack()[0][1]
 res_parts = PurePath(inspect.stack()[0][1]).parts[5:]
 syspaths = sys.path[1:]
 candicate_roots = []
-root = max(candicate_roots, key=len)
 for i in syspaths:
 	if i in res:
 		candicate_roots.append(i)
+root = max(candicate_roots, key=len)
 
 print(candicate_roots)
 print(syspaths)
+print(root)
