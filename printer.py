4fd233cf8025ec628e21bed3c4903bbdb04db55f
John Thehow <johnthehow@qq.com>
2023-03-21 18:18:51 +0800

    regular update

--- printer.py
+++ printer.py
@@ -3,7 +3,7 @@ from pathlib import PurePath
 import sys
 
 def module_ref():
-	res = inspect.stack()[1][1].lower()
+	res = inspect.stack()[1].function.lower()
 	print(res)
 	syspaths = [i.lower() for i in sys.path[1:]]
 	candicate_roots = []
