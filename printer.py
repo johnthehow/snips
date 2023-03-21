a1aeed6fb96f2ec3f5b274c3ca1e211e5b759c44
John Thehow <johnthehow@qq.com>
2023-03-21 18:12:16 +0800

    regular update

--- printer.py
+++ printer.py
@@ -1,19 +1,16 @@
 import inspect
 from pathlib import PurePath
 import sys
-res = inspect.stack()[0][1].lower()
-res_parts = PurePath(inspect.stack()[0][1]).parts[5:]
-syspaths = [i.lower() for i in sys.path[1:]]
-candicate_roots = []
-for i in syspaths:
-	if i in res:
-		candicate_roots.append(i)
-root = max(candicate_roots, key=len)
-remain = res.replace(root,'').replace('\\','/')
-remain_parts = PurePath(remain).parts
-print(candicate_roots)
-print(syspaths)
-print(root)
-print(remain)
-print(remain_parts)
-print('.'.join(remain_parts[1:]))
+
+def module_ref():
+	res = inspect.stack()[0][1].lower()
+	res_parts = PurePath(inspect.stack()[0][1]).parts[5:]
+	syspaths = [i.lower() for i in sys.path[1:]]
+	candicate_roots = []
+	for i in syspaths:
+		if i in res:
+			candicate_roots.append(i)
+	root = max(candicate_roots, key=len)
+	remain = res.replace(root,'').replace('\\','/')
+	remain_parts = PurePath(remain).parts
+print('.'.join(remain_parts[1:])[:-3])
