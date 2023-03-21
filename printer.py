018e2d100a945efeaa038b50999d3b76f7241a5b
John Thehow <johnthehow@qq.com>
2023-03-21 17:51:13 +0800

    regular update

--- printer.py
+++ printer.py
@@ -1,4 +1,6 @@
 import inspect
 from pathlib import PurePath
-res = PurePath(inspect.stack()[0][1]).parts[5:]
+import sys
+res = inspect.stack()[0][1]
+res_parts = PurePath(inspect.stack()[0][1]).parts[5:]
 print(res)
