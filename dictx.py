e6bdf2c19d8ba8b6ad84cb44a1324f780e03d014
John Thehow <johnthehow@qq.com>
2023-03-21 19:13:03 +0800

    regular update

--- dictx.py
+++ dictx.py
@@ -416,7 +416,7 @@ def vacate_dict(dic): # 20230320013251
 		1.
 		2. 
 '''
-def merge_dicts(dicts):
+def merge_dicts(dicts): # 20230321190735
 	keylists = extract_keylists(dicts[0]) # 用于生成空的结果承载嵌套字典的keylist
 	empty_nested_dict = nested_dict(keylists, len(keylists)-1) # # 空的结果承载嵌套字典
 	for i in product(*keylists):
