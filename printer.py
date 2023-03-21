f9e5c6cd82840b1fc5d462fbd20f29e216c6a0af
John Thehow <johnthehow@qq.com>
2023-03-21 17:57:26 +0800

    regular update

--- printer.py
+++ printer.py
@@ -3,4 +3,10 @@ from pathlib import PurePath
 import sys
 res = inspect.stack()[0][1]
 res_parts = PurePath(inspect.stack()[0][1]).parts[5:]
-print(res)
+syspaths = sys.path
+candicate_roots = []
+for i in syspaths:
+	if i in res:
+		cadicate_roots.append(i)
+
+print(candicate_roots)
