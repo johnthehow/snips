ae8e508b63b926add0588739d9637c5448c4b8e5
John Thehow <johnthehow@qq.com>
2023-03-29 13:32:44 +0800

    regular update

--- pathx.py
+++ pathx.py
@@ -5,7 +5,7 @@ def resuffix(filename): # 20230329132438
 	parent = filepath.parent
 	stem = filepath.stem
 	suffix = filepath.suffix
-	resuffix_filename = stem+'.'+suffix+'.'+'proc'
+	resuffix_filename = stem+suffix+'.'+'proc'
 	resuffix_filepath = parent.joinpath(resuffix_filename)
 	return resuffix_filepath
 
