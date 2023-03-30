3de77e02615eb5ec820a35e94823093cabd94b19
John Thehow <johnthehow@qq.com>
2023-03-30 12:57:34 +0800

    regular update

--- forx.py
+++ forx.py
@@ -3,3 +3,7 @@ from itertools import product
 def nested_loop(func,lists): # 20230330125323
 	for idx_seq in product(*lists):
 		print(idx_seq)
+
+
+if __name__ == '__main__':
+	nested_loop('hello', lists)
