54bb74089413ecf593fe9ab6082cf3451bca6cfb
John Thehow <johnthehow@qq.com>
2023-03-30 15:09:21 +0800

    regular update

--- funcx.py
+++ funcx.py
@@ -3,3 +3,9 @@ from thehow.snips.constants import nested_dict_keylist
 def paraseq(func,paralists): # 20230330150547
 	for seq_idx in product(*paralists):
 		print(seq_idx)
+def func(*args):
+	for i in funcs:
+		print(i)
+
+if __name__ == '__main__':
+	paraseq(func, nested_dict_keylist)
