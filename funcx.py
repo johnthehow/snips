60ffa60854d474cdd2004f62bdeaa162d4098966
John Thehow <johnthehow@qq.com>
2023-03-30 15:08:32 +0800

    regular update

--- funcx.py
+++ funcx.py
@@ -1,5 +1,5 @@
 from itertools import product
-
+from thehow.snips.constants import nested_dict_keylist
 def paraseq(func,paralists): # 20230330150547
 	for seq_idx in product(*paralists):
 		print(seq_idx)
