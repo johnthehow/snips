e9cd32733d4b7d214503e9307ca94481a5111c82
John Thehow <johnthehow@qq.com>
2023-03-30 15:22:24 +0800

    regular update

--- funcx.py
+++ funcx.py
@@ -1,35 +1,43 @@
 from itertools import product
 from thehow.snips.constants import nested_dict_keylist
+from thehow.snips.dictx import create_nested_dict
 
 '''
 [函数注释]
 	[功能]
-		1. 主要功能: 将本来需要手动输入一个
+		1. 主要功能: 将本来只能用于一组参数的函数, 变成能用于多组参数的函数
 		2. 额外功能
 	[设计图]
 		1. 索引码: 
 		2. 文件类型: 
 	[参数]
-		1. [参数1]
-			1. 数据类型: 
-			2. 数据结构: 
-			3. 参数类型: 
-			4. 语义: 
+		1. func
+			1. 数据类型: function
+			2. 数据结构: function
+			3. 参数类型: 必选参数
+			4. 语义: 希望变成能用于多组参数的函数的本来只能用于一组参数的函数
 			5. 取值范围: 
-			6. 获得来源: 
-			7. 样例文件/输入: 
-		2. [参数2]
-			1. 数据类型: 
-			2. 数据结构: 
-			3. 参数类型: 
-			4. 语义: 
+			6. 获得来源: 手动输入
+			7. 样例文件/输入: func = lambda f1,f2,v1,v2,v3,v4: print(f1,f2,v1,v2,v3,v4)
+		2. fix_para_list
+			1. 数据类型: iterable
+			2. 数据结构: iterable
+			3. 参数类型: 必选参数
+			4. 语义: 在函数中值固定不变的参数
 			5. 取值范围: 
-			6. 获得来源: 
-			7. 样例文件/输入: 
+			6. 获得来源: 手动输入
+			7. 样例文件/输入: fixpara = ['fix1','fix2']
+		3. nest_para_lists
+			1. 数据类型: list
+			2. 数据结构: [list,list]
+			3. 参数类型: 必选参数
+			4. 语义: 在函数中值不断改变,层层嵌套的参数
+			5. 取值范围: 
+			6. 获得来源: 手动输入
+			7. 样例文件/输入: nested_paras = thehow.snips.constants.nested_dict_keylist
 	[用例]
-		1. [用例1]
-			1. 语句: 
-			2. 输出
+		1. paraseq(func, fixpara, nested_paras)
+			1. 输出
 				1. 语义: 
 				2. 数据类型: 
 				3. 数据结构: 
@@ -52,8 +60,9 @@ from thehow.snips.constants import nested_dict_keylist
 		2. 
 '''
 def paraseq(func, fix_para_list, nest_para_lists): # 20230330150547
+	result = create_nested_dict(nest_para_lists, len(nest_para_lists)-1)
 	for seq_idx in product(*nest_para_lists):
-		func(*fix_para_list,*seq_idx)
+		one_result = func(*fix_para_list,*seq_idx)
 
 
 if __name__ == '__main__':
