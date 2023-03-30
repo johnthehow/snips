fa675fc56fb34a5e8b9e1402f6a1192ca9fd00eb
John Thehow <johnthehow@qq.com>
2023-03-30 13:02:39 +0800

    regular update

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 word_list = ['in', 'for', 'on', 'at', 'by', 'because', 'a', 'as', 'about', 'an', 'from', 'i', 'he', 'him', 'she', 'her', 'if', 'into', 'me', 'of', 'than', 'the', 'they', 'them', 'to', 'this', 'that', 'when', 'where', 'who', 'whom', 'you', 'with', 'which', 'what', 'these', 'those', 'and', 'or', 'but']
 ud_pos = ['NOUN','VERB','ADJ','ADP','DET','PRON','SCONJ','CCONJ','ADV','AUX','PART','PROPN','INTJ','NUM','SYM','PUNCT','X']
 freq_content_word = ['people','so','time','year','years','said','report','new','government']
-
+nested_dict_keylist = [['α','β','γ','δ'],['甲','乙','丙','丁'],['a','b','c','d'],[1,2,3,4]]
 
