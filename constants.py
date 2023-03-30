1e8c85300c7c882402c9ef46b6ceb5787b0a6412
John Thehow <johnthehow@qq.com>
2023-03-30 12:59:03 +0800

    regular update

---
+++ constants.py
@@ -0,0 +1,4 @@
+word_list = ['in', 'for', 'on', 'at', 'by', 'because', 'a', 'as', 'about', 'an', 'from', 'i', 'he', 'him', 'she', 'her', 'if', 'into', 'me', 'of', 'than', 'the', 'they', 'them', 'to', 'this', 'that', 'when', 'where', 'who', 'whom', 'you', 'with', 'which', 'what', 'these', 'those', 'and', 'or', 'but']
+ud_pos = ['NOUN','VERB','ADJ','ADP','DET','PRON','SCONJ','CCONJ','ADV','AUX','PART','PROPN','INTJ','NUM','SYM','PUNCT','X']
+freq_content_word = ['people','so','time','year','years','said','report','new','government']
+
