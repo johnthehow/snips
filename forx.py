ef65a8a1acf1cdaf91739b0fafb8089ba3727def
John Thehow <johnthehow@qq.com>
2023-03-30 13:04:43 +0800

    regular update

--- forx.py
+++ forx.py
@@ -1,6 +1,10 @@
 from itertools import product
 from thehow.snips import constants
 
+def func(*args):
+	for i in args:
+		print(i)
+
 def nested_loop(func,lists): # 20230330125323
 	for idx_seq in product(*lists):
 		print(idx_seq)
