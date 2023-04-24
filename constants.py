b3d7926fa43e8f3e08d6b980b841d4692fe4664e
John Thehow <johnthehow@qq.com>
2023-04-25 02:23:47 +0800

    mod constants.py

--- constants.py
+++
@@ -1,5 +0,0 @@
-word_list = ['in', 'for', 'on', 'at', 'by', 'because', 'a', 'as', 'about', 'an', 'from', 'i', 'he', 'him', 'she', 'her', 'if', 'into', 'me', 'of', 'than', 'the', 'they', 'them', 'to', 'this', 'that', 'when', 'where', 'who', 'whom', 'you', 'with', 'which', 'what', 'these', 'those', 'and', 'or', 'but'] # 20230330134746
-ud_pos = ['NOUN','VERB','ADJ','ADP','DET','PRON','SCONJ','CCONJ','ADV','AUX','PART','PROPN','INTJ','NUM','SYM','PUNCT','X'] # 20230330134748
-freq_content_word = ['people','so','time','year','years','said','report','new','government'] # 20230330134753
-nested_dict_keylist = [['α','β','γ','δ'],['甲','乙','丙','丁'],['a','b','c','d'],[1,2,3,4]] # 20230330134755
-
---
+++ consts.py
@@ -0,0 +1,5 @@
+function_words = ['in', 'for', 'on', 'at', 'by', 'because', 'a', 'as', 'about', 'an', 'from', 'i', 'he', 'him', 'she', 'her', 'if', 'into', 'me', 'of', 'than', 'the', 'they', 'them', 'to', 'this', 'that', 'when', 'where', 'who', 'whom', 'you', 'with', 'which', 'what', 'these', 'those', 'and', 'or', 'but'] # 20230330134746
+content_words = ['people','so','time','year','years','said','report','new','government'] # 20230330134753
+ud_pos = ['NOUN','VERB','ADJ','ADP','DET','PRON','SCONJ','CCONJ','ADV','AUX','PART','PROPN','INTJ','NUM','SYM','PUNCT','X'] # 20230330134748
+nested_dict_keylist = [['α','β','γ','δ'],['甲','乙','丙','丁'],['a','b','c','d'],[1,2,3,4]] # 20230330134755
+
